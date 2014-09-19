abe2f258cea412e30e46e9bd4f7d32744a0e9761
Vinu Isaac <vinuisaacj@outlook.com>
2014-09-19 15:43:18 +0530

    Update line.cpp

--- line.cpp
+++ line.cpp
@@ -1,9 +1,36 @@
-#include <glut.h>
+#include <GL/glut.h>
 #include <math.h>
 #include <stdio.h>
 
 GLint xOne,yOne,Xtwo,yTwo
+
 void resize(int,int);
 void setPixel(GLint,GLint);
 void lineBres_L1(Glint,GLint,GLint,GLint,GLfloat);
-void
+void lineBres_GE1(int,GLint,GLint,GLint,GLfloat);
+void display();
+
+void main(int argc,char** argv)
+{
+  printf("Enter the starting vertex:");
+  scanf("%d%d",&xOne,yOne);
+  printf("Enter the ending vertex:");
+  scanf("%d%d",&xTwo,yTwo);
+ 
+  glutInit(&argc,argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowPosition(800,50);
+  glutInitWindowSize(400,400);
+  glutCreateWindow("Bresnham's Line Algorithm");
+  glutDisplayFunc(display);
+  glutReshapeFunc(resize);
+  glutMainLoop();
+}
+
+void setPixel(int w,int h)
+{
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2d(0.0,w,0.0,h);
+  gl
+}
